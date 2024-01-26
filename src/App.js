8a57c26cc3226e98a185325f355ed9fdb2c2eb83
LuisFCosteC <luisfcostec@gmail.com>
2024-01-26 09:02:25 -0500

    Final Project Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Characters from "./components/Characters";
+import Pagination from "./components/Pagination";
 
 function App() {
   const [characters, setCharacters] = useState([]);
+  const [info, setInfo] = useState({});
 
   const initialUrl = "https://rickandmortyapi.com/api/character";
 
@@ -11,8 +13,19 @@ function App() {
     //** Function to consume api information
     fetch(url)
       .then((response) => response.json())
-      .then((data) => setCharacters(data.results))
+      .then((data) => {
+        setCharacters(data.results);
+        setInfo(data.info);
+      })
       .catch((error) => console.log(error));
+  };
+
+  const onPrevious = () => {
+    fetchCharacters(info.prev);
+  };
+
+  const onNext = () => {
+    fetchCharacters(info.next);
   };
 
   useEffect(() => {
@@ -23,8 +36,20 @@ function App() {
     <>
       <Navbar brand="Rick and Morty App" />
 
-      <div className="container">
+      <div className="container mt-5">
+        <Pagination
+          prev={info.prev}
+          next={info.next}
+          onPrevious={onPrevious}
+          onNext={onNext}
+        />
         <Characters characters={characters} />
+        <Pagination
+          prev={info.prev}
+          next={info.next}
+          onPrevious={onPrevious}
+          onNext={onNext}
+        />
       </div>
     </>
   );
